079df3dad4b8ccfd127d82ebcb766abcfcb146a3
Arshath000 <arshathahamed2710@gmail.com>
2025-02-26 16:26:24 +0530

    upload

---
+++ Demo.java
@@ -0,0 +1,134 @@
+
+
+
+
+/**class Demo{
+    public static void main(String[] args){
+        int x=5;
+        int y=3;
+        System.out.println(x+y);
+    }
+    }*/
+    /**class Demo{
+        public static void main(String[] args){
+            int x=5;
+            int y=3;
+            System.out.println(x-y);
+        }
+        }*/
+        /**class Demo{
+            public static void main(String[] args){
+                int x=5;
+                int y=3;
+                System.out.println(x*y);
+            }
+            }*/
+            /**class Demo{
+                public static void main(String[] args){
+                    int x=5;
+                    int y=3;
+                    System.out.println(x==y);
+                }
+                }*/
+               /**class Demo{
+                    public static void main(String[] args){
+                        int x=5;
+                        int y=3;
+                        System.out.println(x>y);
+                    }
+                    }*/
+                 /**class Demo{
+                        public static void main(String[] args){
+                            int num=15;
+                            int a=3;
+                           int b=5;
+                        if((num%a==0)&&(num%b==0))
+                            {
+                                System.out.println("divisible");
+                            }
+                        else
+                        {
+                            System.out.println("not divisible");
+                        }
+                    }
+                }*/
+                /**class Demo{
+                    public static void main(String[] args){
+                        int a=5;
+                        int b=7;
+                        int c=a&b;
+                        System.out.println("a&b =" +(a&b));
+                    }
+                }*/
+                /**class Demo{
+                    public static void main(String[] args){
+                        int a=5;
+                        int b=7;
+                        int c=a&b;
+                        System.out.println("a|b =" +(a | b));
+                    }
+                }*/
+                /**class Demo{
+                    public static void main(String[] args){
+                        int a=5;
+                        int b=7;
+                        int c=a&b;
+                        System.out.println("a^b =" +(a^b));
+                    }
+                }*/
+
+/**class Demo{
+                    public static void main(String[] args){
+                        int num=14;
+                        if(num>0)
+                        {
+                            System.out.println("positive");
+                        }
+                        else if(num<0)
+                        {
+                            System.out.println("negative");
+
+                        }
+                        else
+                        {
+                            System.out.println("zero");
+                        }
+                    }
+                }*/
+
+import java.util.Scanner;
+
+                class Demo{                     
+                    public static void main(String[] args) {                         
+                        Scanner obj = new Scanner(System.in);                         
+                        int marks = obj.nextInt();                         
+                        System.out.println("Enter: " + marks);                         
+                
+                        if (marks >= 90 && marks <= 100)
+                        { 
+                            System.out.println("A grade");                         
+                        } else if (marks >= 80 && marks <= 89) 
+                        {                         
+                            System.out.println("B grade");                         
+                        } else if (marks >= 70 && marks <= 79) 
+                        {                         
+                            System.out.println("C grade");                         
+                        } else if (marks >= 60 && marks <= 69) 
+                        {                         
+                            System.out.println("D grade");                         
+                        } else {                         
+                            System.out.println("Fail");                         
+                        }                     
+                    }                 
+                }
+                    
+                
+
+
+
+
+                        
+                    
+                            
+                        
+                    
